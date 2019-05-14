909e34831f02bf440de4483b88743246664880e8
humbleafrica <kofiasiedu@hotmail.co.uk>
2019-05-14 11:30:21 +0000

    adding Procfile

--- thecookbook/settings.py
+++ thecookbook/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
+
+
+#heroku settings for django
+django_heroku.settings(locals())
